58ff08f76845e9cb82b6b3a6e0323c2eaa2d24a2
Vikram Reddy <gvreddy04@gmail.com>
2023-04-21 23:28:26 +0530

    .NET 6 - Blazor Bootsrap - WebAssembly Template

--- src/templates/net6/BlazorBootstrapWebAssemblyApp/Program.cs
+++ src/templates/net6/BlazorBootstrapWebAssemblyApp/Program.cs
@@ -1,11 +1,13 @@
+using BlazorBootstrap;
+using BlazorBootstrapWebAssemblyApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorBootstrapWebAssemblyApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorBootstrap();
 
 await builder.Build().RunAsync();
