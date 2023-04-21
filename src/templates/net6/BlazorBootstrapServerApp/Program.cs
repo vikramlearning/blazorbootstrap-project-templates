2d1b68b6dbe53959f631af3df1c72c79ecfe7daf
Vikram Reddy <gvreddy04@gmail.com>
2023-04-22 00:21:42 +0530

    Blazor Bootstrap - Server App (.NET 6) - Project Template

--- src/templates/net6/BlazorBootstrapServerApp/Program.cs
+++ src/templates/net6/BlazorBootstrapServerApp/Program.cs
@@ -1,6 +1,5 @@
+using BlazorBootstrap;
 using BlazorBootstrapServerApp.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddBlazorBootstrap();
 
 var app = builder.Build();
 
