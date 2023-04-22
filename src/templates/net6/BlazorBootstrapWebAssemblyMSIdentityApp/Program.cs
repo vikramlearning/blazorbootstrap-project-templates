aeda68869889c76fad885a3fd3b37b679489777e
Vikram Reddy <gvreddy04@gmail.com>
2023-04-22 22:17:11 +0530

    .NET 6 - WebAssembly MSI - template updates

--- src/templates/net6/BlazorBootstrapWebAssemblyMSIdentityApp/Program.cs
+++ src/templates/net6/BlazorBootstrapWebAssemblyMSIdentityApp/Program.cs
@@ -1,3 +1,4 @@
+using BlazorBootstrap;
 using BlazorBootstrapWebAssemblyMSIdentityApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,9 +8,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddMsalAuthentication(options =>
 {
+    options.ProviderOptions.LoginMode = "redirect"; // https://docs.microsoft.com/en-us/aspnet/core/blazor/security/webassembly/standalone-with-azure-active-directory?view=aspnetcore-6.0#login-mode
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
 });
 
